ef3b0b2f2da8ea93874f7d2c0b19e69ac6361944
hieublack0912 <79302772+hieublack0912@users.noreply.github.com>
2021-02-21 14:28:46 +0700

    ket noi db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,24 @@
-class SiteController {
-  index(req, res) {
-    //[Get] /home
-    res.render('home');
-  }
+const Course = require('../models/Course');
 
-  //[GET] /search
-  search(req, res) {
-    res.render('search');
-  }
+class SiteController {
+    //[Get] /
+    index(req, res) {
+
+        Course.find({}, function(err, courses) {
+            if (!err) {
+                res.json(courses);
+            } else {
+                res.status(400).json({ error: 'Error!!!' });
+            }
+        });
+
+        // res.render('home');
+    }
+
+    //[GET] /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+//conect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,17 +20,17 @@ app.use(morgan('combined'));
 
 //template engine
 app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-  }),
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
